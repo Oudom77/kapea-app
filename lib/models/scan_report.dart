af5f1e47d580f183094a399fd556b7da26ad87a4
Oudom77 <gatorman4511@gmail.com>
2026-07-19 11:03:14 +0700

    update: model, feature: dto for scan report

--- lib/models/scan_report.dart
+++ lib/models/scan_report.dart
@@ -9,8 +9,9 @@ class ScanReport {
   final DateTime scannedAt;
   final List<String> redirectChain;
   final List<String> reasons;
-  final int engineFlagged;
+  final int enginesFlagged;
   final int totalEngines;
+  final String? screenshotUrl;
 
   const ScanReport({
     required this.url,
@@ -18,7 +19,8 @@ class ScanReport {
     required this.scannedAt,
     required this.redirectChain,
     required this.reasons,
-    required this.engineFlagged,
+    required this.enginesFlagged,
     this.totalEngines = 72,
+    this.screenshotUrl,
   });
 }
